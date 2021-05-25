4e736cc77fd7e6aa4b744ec388505d4812ca1366
TimGAllan <59513050+TimGAllan@users.noreply.github.com>
2021-05-25 22:20:20 +0100

    Created Class_Game

---
+++ POB/Class_Game.py
@@ -0,0 +1,53 @@
+import pygame as pg
+import sys
+import os
+
+class Game(object):
+    
+    def __init__(self,player,dungeonview):
+        self.player = player
+        self.dungeonview = dungeonview
+        self.WH = (960,600)
+
+    def Launch(self):
+        ##Initialise Pygame
+        pg.init()
+        pg.font.init()
+
+        ##The Pygame Window to the POB Icon and Label the Pygame Window "Py of th Beholder
+        pg.display.set_icon(pg.image.load('Assets/eob_icon.png'))
+        pg.display.set_caption('Py of the Beholder')
+
+        self.window = pg.display.set_mode(self.WH)
+
+        self.clock = pg.time.Clock()
+
+    def quit(self):
+        pg.quit()
+        sys.exit(0)
+
+    def Tick(self):
+        self.clock.tick(60)
+
+    def redrawWindow(self):
+
+        #  Render the background of the view window        
+        BG = pg.transform.scale(pg.image.load(os.path.join('Assets/Environments', self.dungeonview.Environment, self.dungeonview.bg)), (528,360))    
+        self.window.blit(BG, (0,0))
+    
+        # Render all the wall panels and environment
+        for panel in self.dungeonview.panels:
+            # Redender the Wall panel
+            img = pg.transform.scale(pg.image.load(os.path.join(self.dungeonview.Walls_path, self.dungeonview.tiles[panel], self.dungeonview.panelImageFilenames[panel])), self.dungeonview.panelsWidthHeights[panel])
+            self.window.blit(img, self.dungeonview.panelsPositions[panel])
+
+            # Render the Adornment
+            img = pg.transform.scale(pg.image.load(os.path.join(self.dungeonview.Adornments_path, self.dungeonview.Adornments_panels[panel], self.dungeonview.panelImageFilenames[panel])), self.dungeonview.panelsWidthHeights[panel])
+            self.window.blit(img, self.dungeonview.panelsPositions[panel])          
+
+        # render the UI
+        UI = pg.transform.scale(pg.image.load(os.path.join('Assets', 'UI', self.player.D + '.png')), self.WH)
+        self.window.blit(UI, (0,0))
+    
+        # display it in pygame
+        pg.display.update()
--- POB/Main.py
+++ POB/Main.py
@@ -7,67 +7,30 @@ import sys
 from Dungeon            import *
 from Class_DungeonView  import *
 from Class_Player       import *
+from Class_Game         import *
 from UIConstants        import *
-
-
-##Initialis Pygame
-pg.init()
-pg.font.init()
-
-##The Pygame Window to the POB Icon and Label the Pygame Window "Py of th Beholder
-programIcon = pg.image.load('Assets/eob_icon.png')
-pg.display.set_icon(programIcon)
-pg.display.set_caption('Py of the Beholder')
-
-
-WIN = pg.display.set_mode((960, 600))
 
 dungeonView = DungeonView(Dungeon.levels[0].Environment)
 player = Player(7,13,'E')
 
-def quitGame():
-    pg.quit()
-    sys.exit(0)
+game = Game(player,dungeonView)
+game.Launch()
 
-def redrawWindow():
-
-    #  Render the background of the view window        
-    BG = pg.transform.scale(pg.image.load(os.path.join('Assets/Environments', dungeonView.Environment, dungeonView.bg)), BG_WH)    
-    WIN.blit(BG, ORIGIN_POS)
-    
-    # Render all the wall panels and environment
-    for panel in dungeonView.panels:
-        # Redender the Wall panel
-        img = pg.transform.scale(pg.image.load(os.path.join(dungeonView.Walls_path, dungeonView.tiles[panel], dungeonView.panelImageFilenames[panel])), dungeonView.panelsWidthHeights[panel])
-        WIN.blit(img, dungeonView.panelsPositions[panel])
-
-        # Render the Adornment
-        img = pg.transform.scale(pg.image.load(os.path.join(dungeonView.Adornments_path, dungeonView.Adornments_panels[panel], dungeonView.panelImageFilenames[panel])), dungeonView.panelsWidthHeights[panel])
-        WIN.blit(img, dungeonView.panelsPositions[panel])          
-
-    # render the UI
-    UI = pg.transform.scale(pg.image.load(os.path.join('Assets', 'UI', player.D + '.png')), (WIDTH, HEIGHT))
-    WIN.blit(UI, ORIGIN_POS)
-    
-    # display it in pygame
-    pg.display.update()
 
 def main():
     
-    clock = pg.time.Clock()
-    
     while True:
-        clock.tick(FPS)
+        game.Tick()
         dungeonView.updatePanels(player.position,Dungeon.levels[0].WallsX,Dungeon.levels[0].WallsY,Dungeon.levels[0].Adornments,Dungeon.levels[0].Clipping)
-        redrawWindow()
+        game.redrawWindow()
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
-                quitGame()
+                game.quit()
                 
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
-                    quitGame() 
+                    game.quit() 
                 
                 if pg.key.name(event.key) in 'qweasd':
                     player.move(Dungeon.levels[0].Clipping,pg.key.name(event.key))
